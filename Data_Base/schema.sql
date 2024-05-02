785210d73130e58657a71733ca8b04d95cb1bc42
sofia-lpz <a01028251@tec.mx>
2024-05-02 09:10:05 -0600

    added player data, wip

--- Data_Base/schema.sql
+++ Data_Base/schema.sql
@@ -3,12 +3,18 @@ use colometa;
 
 create table player
 (
-
+username varchar(50),
+level int,
+score int,
+email varchar(50),
+password varchar(50)
+primary key(username)
 );
 
 create table card
 (
-
+name varchar(50) primary key,
+type varchar(50),
 );
 
 create table match
