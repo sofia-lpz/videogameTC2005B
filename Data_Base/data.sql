38e79e0f10d106afc9ace4de7ecb1b0c44792db4
sofia-lpz <a01028251@tec.mx>
2024-05-02 14:49:02 -0600

    added card ideas

---
+++ Data_Base/data.sql
@@ -0,0 +1,51 @@
+use colmeta;
+
+insert into player (username, password, level, score) values 
+('admin', 'admin', 1, 0),
+('player', 'password', 1, 0);
+
+
+-- ideas for cards
+-- 24 cards minimum
+/*
+4 healing cards
+4 support cards
+8 attack cards
+8 defense cards
+
+
+'creature', 'hypnosis', 'snack', 'weapon', 'tool', 'skill', 'objects'
+
+snack
+- candy
+- apple
+- chocolate
+- plum juice
+- sandwich
+- peach
+- jam
+
+Weapons
+- sharp stick
+- sword
+- pocket knife
+
+hypnosis
+- sleep
+- confusion
+- turn into chicken
+- fear
+
+creature
+- beetle
+- snake
+- spider
+- rat
+- bat
+
+objects
+- 4 leaf clover (is lucky)
+- 3 leaf clover
+
+
+*/
