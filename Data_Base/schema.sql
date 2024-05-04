80f8b1d1710c64ac0c5856cfbadc64b6bc645a41
sofia-lpz <a01028251@tec.mx>
2024-05-04 11:06:00 -0600

    wip

--- Data_Base/schema.sql
+++ Data_Base/schema.sql
@@ -26,6 +26,8 @@ matchId varchar(15) NOT NULL DEFAULT 'match' AUTO_INCREMENT,
 timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 player varchar(50) NOT NULL,
 -- add player health?
+-- add card used by player?
+-- add number of turns?
 won boolean NOT NULL,
 primary key (matchId)
 foreign key (player1) references player(username),
@@ -58,5 +60,21 @@ CREATE VIEW PlayerDeck AS
 SELECT player, GROUP_CONCAT(card) as Cards
 FROM deck
 GROUP BY player;
+
+
+create trigger update_wins
+after insert on match
+for each row
+begin
+    if new.won = true then
+        update player
+        set matches_won = matches_won + 1
+        where username = new.player;
+    end if;
+end;
+
+create trigger update_deck
+-- after win, add the npc card to the player's deck
+
 
 
