7d47ffff7e3d79a309e3e690ff36a39fb729ee00
sofia-lpz <a01028251@tec.mx>
2024-05-04 19:59:51 -0600

    added fonts, added stateName

---
+++ Videogame/LostIllusion/Assets/Scripts/stateNameController.cs
@@ -0,0 +1,21 @@
+/*
+File to hold variables across scenes
+
+Sofia Moreno
+4/5/2024
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stateNameController : MonoBehaviour
+{
+    // To use, call stateNameController.theNameOfYourVariable
+    // and access it from any script in any scene you want
+
+
+    //for example in this format
+    [SerializeField] public static string scoreGameOver = "0";
+}
+
