28fda4608fbce86de5050a9d6bda4026b2cdc657
sofia-lpz <a01028251@tec.mx>
2024-05-05 02:24:10 -0600

    added test dialogue

---
+++ Videogame/LostIllusion/Assets/Scripts/dialogue.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dialogue : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] string[] lines;
+    [SerializeField] float textSpeed = 0.05f;
+    [SerializeField] int index;
+    AudioSource audioSource;
+    [SerializeField] float minPitch = 0.9f;
+    [SerializeField] float maxPitch = 1.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Resources.Load<AudioClip>("Audio/soundEffects/text");
+        textComponent.text = "";
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            if (c != ' '){
+                audioSource.pitch = Random.Range(0.9f, 1.1f);
+                audioSource.Play();
+            }
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = "";
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            textComponent.text = "";
+        }
+    }
+}
+
