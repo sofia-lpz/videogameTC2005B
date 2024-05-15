22fb8e5dc94186a53ded3638a61b7b690ed18710
sofia-lpz <a01028251@tec.mx>
2024-05-15 11:18:23 -0600

    added some views

--- Data_Base/views.sql
+++ Data_Base/views.sql
@@ -11,6 +11,26 @@ SELECT player, GROUP_CONCAT(card) as Cards
 FROM deck
 GROUP BY player;
 
+CREATE VIEW PlayerTeam AS
+SELECT player, GROUP_CONCAT(villager) as Team
+FROM team
+GROUP BY player;
+
+CREATE VIEW PlayerStats AS
+SELECT player, most_used_card, most_used_villager, least_used_card, least_used_villager, found_objects
+FROM stats
+GROUP BY player;
+
+CREATE VIEW PlayerMatches AS
+SELECT player, COUNT(*) as Matches
+FROM match
+GROUP BY player;
+
+CREATE VIEW VillagerTeam AS
+--which players are teammates with each villager
+SELECT villager, GROUP_CONCAT(player) as Team
+
+
 create trigger update_wins
 after insert on match
 for each row
