f5a0b7a882c2963c5f4f0cd9233546452048cec4
sofia-lpz <a01028251@tec.mx>
2024-05-17 00:05:29 -0600

    added elipsis pause

--- Videogame/LostIllusion/Assets/Scripts/dialogue/dialogueData.cs
+++ Videogame/LostIllusion/Assets/Scripts/dialogue/dialogueData.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 
 public class dialogueData : stateNameController
 {
+/*TV LINES*/
        public static List<string> tv_lines_1 = new List<string> {
         "Hola " + player_username,
         "QUE?",
@@ -28,21 +29,44 @@ public class dialogueData : stateNameController
         "ve a buscar mas cartas en el mapa",
         "y vuelve a intentarlo"
     };
+
+        public static List<List<string>> tv_lines = new List<List<string>> {
+    tv_lines_1,
+    tv_lines_won,
+    tv_lines_lost
+};
+
+
+/*TV LINES END*/
+
+/*TV2TALK LINES*/
     public static List<string> tv2talk_lines = new List<string> {
         "Hola " + player_username,
         "Estoy aqui para platicar en el index 0",
         "bla bla bla",
     };
+/*TV2TALK LINES END*/
+
+/*BOOK DESCRIPTION*/
     public static List<string> bookDescription = new List<string> {
         "You've picked up a book",
         "neat!",
     };
+/*BOOK DESCRIPTION END*/
 
-    public static List<List<string>> tv_lines = new List<List<string>> {
-    tv_lines_1,
-    tv_lines_won,
-    tv_lines_lost
+
+/*COLMETA LINES*/
+public static List<string> colometa_lines_1 = new List<string> {
+    "3... 2... 1...",
+    "ah!",
+    "Now Adria, tell me...",
+    "...Do you feel hypnotized?"
 };
+
+public static List<List<string>> colometa_lines = new List<List<string>> {
+    colometa_lines_1
+};
+/*COLMETA LINES END*/
 
 
 // to do, make a dictionary of dictionaries instead of dictionary of lists
@@ -50,6 +74,7 @@ public static Dictionary<string, List<List<string>>> dialoguesDictionary = new D
     {"tv", tv_lines},
     {"talk2tv", new List<List<string>> { tv2talk_lines }},
     {"book", new List<List<string>> { bookDescription }},
+    {"COLOMETA", colometa_lines}
 };
 
 }
