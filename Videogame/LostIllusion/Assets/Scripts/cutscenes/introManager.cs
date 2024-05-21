489dab2cd266e17d26c1e2288b13748f424db2c9
sofia-lpz <a01028251@tec.mx>
2024-05-21 17:51:08 -0600

    added cutscene canvas prefab and updated story

--- Videogame/LostIllusion/Assets/Scripts/cutscenes/introManager.cs
+++ Videogame/LostIllusion/Assets/Scripts/cutscenes/introManager.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cutsceneManager : MonoBehaviour
+public class introManager : MonoBehaviour
 {
-
     [SerializeField] public GameObject dialogueCanvasPrefab;
-    
+    [SerializeField] public GameObject CutsceneCanvasPrefab;
 
     void Start()
     {
         GameObject dialogueCanvas = Object.Instantiate(dialogueCanvasPrefab);
         dialogueCanvas.GetComponent<dialogue>().Initialize("COLOMETA", 0);
 
+        GameObject cutsceneCanvas = Object.Instantiate(CutsceneCanvasPrefab);
+        cutsceneCanvas.GetComponent<dialogue>().Initialize("NARRATOR", 0);
+
     }
-    
+
+    // Update is called once per frame
     void Update()
     {
         if (stateNameController.gamePaused){
