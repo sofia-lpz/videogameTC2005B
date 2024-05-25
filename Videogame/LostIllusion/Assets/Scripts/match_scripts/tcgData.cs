daf8e11b1f3dab38cdf784466420c174385d4a16
sofia-lpz <a01028251@tec.mx>
2024-05-25 17:43:03 -0600

    added tcgData

--- Videogame/LostIllusion/Assets/Scripts/match_scripts/tcgData.cs
+++ Videogame/LostIllusion/Assets/Scripts/match_scripts/tcgData.cs
@@ -1,26 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class tcgData : MonoBehaviour
+[System.Serializable]
+public class TCGData
 {
-}
-
-public class cardsData
-{
-    public static int cardId;
-    public static string name;
-    public static int energy_cost;
-    public static string effect;
-    public static string type;
-    public static string description;
-
-    public static int player_health;
-    public static int player_attack;
-    public static int player_support;
-    public static int enemy_defense;
-}
-
-public class villagerData
-{
+    public static int cardID;
+    public static int energyCost;
 }
