eb6680c464922a15d159df891076ae49a87a18ff
javiermontero41 <a01026213@tec.mx>
2024-05-27 10:04:08 -0600

    Actualizacion frontend

--- website/app.js
+++ website/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("html");
 });
 
 app.listen(port, () => {
