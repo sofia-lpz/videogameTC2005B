1a316755877e38b7aabf7164aa716c49bccf70cd
sofia-lpz <a01028251@tec.mx>
2024-05-29 19:25:01 -0600

    added some views

---
+++ Data_Base/noconflicts_views.sql
@@ -0,0 +1,18 @@
+create view player_team
+select v.name, v.description, v.element from villager as v
+join team as t on v.name = t.villager
+join player as p on t.username = p.username;
+
+create view player_tcg_match
+select m.matchId, m.timestamp, m.username, m.won from tcg_match as m
+join player as p on m.username = p.username;
+
+create view player_stats
+select s.username, s.most_used_card, s.most_used_villager, s.least_used_card, s.least_used_villager, s.found_objects from stats as s
+join player as p on s.username = p.username;
+
+
+create view player_deck
+select d.username, c.cardId, c.name, c.energy_cost, c.effect, c.type, c.description from deck as d
+join card as c on d.cardId = c.cardId
+join player as p on d.username = p.username;
