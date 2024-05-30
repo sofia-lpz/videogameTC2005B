f97dd019ef1760dd5c71b72a5c58ea2fb002e73c
Javier Montero <a01026213@tec.mx>
2024-05-30 10:03:22 -0600

    Correccion frontend

--- website/app.js
+++ website/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 import express from "express";
@@ -11,25 +10,12 @@ const port = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-=======
-import express from "express";
-import cors from "cors";
-const app = express();
-const port = 3001;
->>>>>>> b4079ad304fa6f5fb74fb41013968892fcd059fc
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
 
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, 'public')));
-=======
-app.use(express.static('public'));
-
-app.use(cors());
-
->>>>>>> b4079ad304fa6f5fb74fb41013968892fcd059fc
 
 app.get("/", (req, res) => {
   res.render("html");
