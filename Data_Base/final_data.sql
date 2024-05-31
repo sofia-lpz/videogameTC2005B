b708158a8e8ceedebf061186f6ee42fbe6ce5681
luisda25 <luisdaf04@outlook.com>
2024-05-31 11:02:54 -0600

    added final data for data base

--- Data_Base/final_data.sql
+++ Data_Base/final_data.sql
@@ -19,31 +19,31 @@ insert into deck (username, cardId) values
 ('admin1', '3');
 
 
-insert into card (cardId, name, energy_cost, effect, type, description, player_health, player_attack, player_defense, player_support, enemy_defense) values
-('1', 'plum juice', 3, 'healing', 'snack', 'drinking this grants 3 hp', 3, 0, 0, 0, 0),
-('2', 'plum jam', 1, 'healing', 'snack', 'eating this grants 1 hp', 1, 0, 0, 0, 0),
-('3', 'candy', 2, 'healing', 'snack', 'this candy grants 2 hp', 2, 0, 0, 0, 0),
-('4', 'bandage', 3, 'healing', 'skill', 'apply bansage and heal your both characters 2 hp', 2, 0, 0, 0, 0),
-('5', 'glove', 3, 'support', 'skill', 'applies immunity to the element damage for 1 round'),
-('6', '4 leaf clover', 1, 'support', 'skill', 'increases wind attack by 1 point'),
-('7', 'friendship bracelet', 1, 'support', 'skill', 'increases defense by 1 point'),
-('8', 'horse shoe', 2, 'support', 'skill', 'increases attack by 2 points'),
-('9', 'chocolate', 3, 'support', 'snack', 'decreases the enemy accuracy for 1 round'),
-('10', 'dog', 2, 'support', 'creature', 'increases accuracy for 1 round'),
-('11', 'sword', 2, 'attack', 'weapon', 'deals 2 damage to your opponent'),
-('12', 'fear', 3, 'attack', 'hypnosis', 'deals 1 damage to the opponent and increases defense by 1 point'),
-('13', 'sharp stick', 1, 'attack', 'weapon', 'deals 1 damage to your opponent'),
-('14', 'sleep', 2, 'attack', 'hypnosis', 'skips your opponents turn'),
-('15', 'confusion', 2, 'attack', 'skill', 'deals 1 damage each turn for 2 turns'),
-('16', 'mock', 1, 'attack', 'skill', 'decreases the enemy attack by 1 point'),
-('17', 'hide', 2, 'defense', 'skill', 'decreases the enemy accuracy by 1 point'),
-('18', 'stare', 1, 'defense', 'skill', 'decreases the enemy attack by 1 point'),
-('19', 'block', 2, 'defense', 'skill', 'decreases the enemy attack by 2 points'),
-('20', 'dandelion', 3, 'defense', 'skill', 'decreases the enemy attack by 3 points'),
-('21', 'seashell', 2, 'defense', 'skill', 'decreases the enemy attack by 2 points'),
-('22', 'rabbit foot', 1, 'defense', 'skill', 'decreases the enemy attack by 1 point'),
-('23', 'wedding ring', 3, 'defense', 'skill', 'decreases the enemy attack by 3 points'),
-('24', 'wishbone', 2, 'defense', 'skill', 'decreases the enemy attack by 2 points');
+insert into card (cardId, name, energy_cost, effect, type, description, player_health, player_attack, player_defense, player_support, enemy_defense, enemy_attack) values
+('1', 'plum juice', 3, 'healing', 'snack', 'drinking this grants 3 hp', 3, 0, 0, 0, 0, 0),
+('2', 'plum jam', 1, 'healing', 'snack', 'eating this grants 1 hp', 1, 0, 0, 0, 0, 0),
+('3', 'candy', 2, 'healing', 'snack', 'this candy grants 2 hp', 2, 0, 0, 0, 0, 0),
+('4', 'bandage', 3, 'healing', 'skill', 'apply bansage and heal your both characters 2 hp', 2, 0, 0, 0, 0, 0),
+('5', 'glove', 3, 'support', 'skill', 'applies increases defense by 3 points', 0, 0, 3, 0, 0, 0),
+('6', '4 leaf clover', 1, 'support', 'skill', 'increases attack by 1 point', 0, 0, 0, 1, 0, 0),
+('7', 'friendship bracelet', 1, 'support', 'skill', 'increases defense by 1 point', 0, 0, 1, 0, 0, 0),
+('8', 'horse shoe', 2, 'support', 'skill', 'increases attack by 2 points', 0, 0, 0, 2, 0, 0),
+('9', 'chocolate', 3, 'support', 'snack', 'lowers enemy defense by 1 point', 0, 0, 0, 0, 1, 0),
+('10', 'dog', 2, 'support', 'creature', 'lowers enemy defense by 2 points', 0, 0, 0, 0, 2, 0),
+('11', 'sword', 2, 'attack', 'weapon', 'deals 2 damage to your opponent', 0, 2, 0, 0, 0, 0),
+('12', 'fear', 3, 'attack', 'hypnosis', 'deals 1 damage to the opponent and increases defense by 1 point', 0, 1, 1, 0, 0, 0),
+('13', 'sharp stick', 1, 'attack', 'weapon', 'deals 1 damage to your opponent', 0, 1, 0, 0, 0, 0),
+('14', 'sleep', 2, 'attack', 'hypnosis', 'skips your opponents turn', 0, 0, 0, 0, 0, 0),
+('15', 'confusion', 2, 'attack', 'skill', 'deals 2 damage to your opponent', 0, 2, 0, 0, 0, 0),
+('16', 'mock', 3, 'attack', 'skill', 'decreases the enemy attack by 2 points', 0, 0, 0, 0, 0, 2),
+('17', 'hide', 1, 'defense', 'skill', 'increases defense by 1 point', 0, 0, 1, 0, 0, 0),
+('18', 'stare', 1, 'defense', 'skill', 'decreases the enemy attack by 1 point', 0, 0, 0, 0, 0, 1),
+('19', 'block', 2, 'defense', 'skill', 'decreases the enemy attack by 2 points', 0, 0, 0, 0, 0, 2),
+('20', 'dandelion', 3, 'attack', 'skill', 'deals 3 damage points to opponent', 0, 3, 0, 0, 0, 0),
+('21', 'seashell', 1, 'attack', 'skill', 'deals 1 damage to opponent', 0, 1, 0, 0, 0, 0),
+('22', 'rabbit foot', 1, 'healing', 'skill', 'heals 1 health point', 1, 0, 0, 0, 0, 0),
+('23', 'wedding ring', 3, 'healing', 'skill', 'heals 2 health points', 2, 0, 0, 0, 0, 0),
+('24', 'wishbone', 2, 'attack', 'skill', 'deals 3 damage', 0, 3, 0, 0, 0, 0);
 
 
 insert into team (username, villager) values
