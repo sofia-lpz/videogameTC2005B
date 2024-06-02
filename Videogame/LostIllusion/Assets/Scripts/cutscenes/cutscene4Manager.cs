fee57a119563c4d90b910e9c9ccf61e399ab1649
sofia-lpz <a01028251@tec.mx>
2024-06-01 23:25:54 -0600

    removed unused variables, made all cutscene scripts inherit main cutsceneManager

--- Videogame/LostIllusion/Assets/Scripts/cutscenes/cutscene4Manager.cs
+++ Videogame/LostIllusion/Assets/Scripts/cutscenes/cutscene4Manager.cs
@@ -14,105 +14,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class cutscene4Manager : MonoBehaviour
+public class cutscene4Manager : cutsceneManager
 {
-    [SerializeField] public GameObject dialogueCanvasPrefab;
-    [SerializeField] public GameObject CutsceneCanvasPrefab;
-    [SerializeField] public GameObject fadeOutCanvasPrefab;
-    [SerializeField] public string nextScene = "Outside";
-    private GameObject cutsceneCanvas; // Declare cutsceneCanvas here
-    private int introIndex = 0;
-
-    void Start()
-    {
-        StartCoroutine(scene());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (stateNameController.gamePaused){
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            cutsceneCanvas = Instantiate(fadeOutCanvasPrefab);
-        stateNameController.playedCutscenes.Add("cutscene4");
-            SceneManager.LoadScene(nextScene);
-        }
-
-    }
-
-    public IEnumerator scene()
+    
+    public override IEnumerator scene()
     {
         cutsceneCanvas = Instantiate(CutsceneCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("NARRATOR", 8);
     
-        // Wait until the first canvas has been destroyed
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
-
-
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(dialogueCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("COLOMETA", 6);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(CutsceneCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("NARRATOR", 9);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(dialogueCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("COLOMETA", 7);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(CutsceneCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("NARRATOR", 10);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(dialogueCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("COLOMETA", 8);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
+        yield return waitUntilCanvasDestroyed();
 
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
         cutsceneCanvas = Instantiate(CutsceneCanvasPrefab);
         cutsceneCanvas.GetComponent<dialogue>().Initialize("NARRATOR", 11);
 
-        while (cutsceneCanvas != null)
-        {
-            yield return null; // Wait for the next frame
-        }
-
-        yield return new WaitForSeconds(1.0f); // Wait for 1 second
+        yield return waitUntilCanvasDestroyed();
+        
         cutsceneCanvas = Instantiate(fadeOutCanvasPrefab);
         stateNameController.playedCutscenes.Add("cutscene4");
         SceneManager.LoadScene(nextScene);
