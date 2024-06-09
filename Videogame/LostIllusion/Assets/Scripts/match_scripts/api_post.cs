153edac1f78661a663c193d36bdf950ec72d3757
sofia-lpz <a01028251@tec.mx>
2024-06-09 16:45:16 -0600

    added post for matches
    Co-authored-by: sofia-lpz <a01028251@tec..mx>
    Co-authored-by: luisda25 <luisdaf04@outlook.com>

--- Videogame/LostIllusion/Assets/Scripts/match_scripts/api_post.cs
+++ Videogame/LostIllusion/Assets/Scripts/match_scripts/api_post.cs
@@ -7,9 +7,14 @@ public class api_post : MonoBehaviour
 {
 
 //para llamarlo cuando se acabe la match de acuerdo al tcg_controller
-    public void postData(string mostUsedCard, string mostUsedVillager, string leastUsedCard, string leastUsedVillager, int memoriesFound)
+    public static void postData(string mostUsedCard, string mostUsedVillager, string leastUsedCard, string leastUsedVillager, int memoriesFound)
     {
         StartCoroutine(PostStatsData(stateNameController.Players[0].username, mostUsedCard, mostUsedVillager, leastUsedCard, leastUsedVillager, memoriesFound));
+    }
+
+    public static void postMatchData(bool won)
+    {
+        StartCoroutine(PostMatchesData(stateNameController.Players[0].username, won));
     }
 
 
@@ -39,4 +44,29 @@ IEnumerator PostStatsData(string username, string mostUsedCard, string mostUsedV
         }
     }
 }
+
+IEnumerator PostMatchesData(string username, bool won)
+{
+    string wonstring = won ? "true" : "false";
+
+    WWWForm form = new WWWForm();
+    form.AddField("username", username);
+    form.AddField("won", wonstring);
+
+    using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:3000/api/create/matches", form))
+    {
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Request failed: " + www.error);
+        }
+        else
+        {
+            string result = www.downloadHandler.text;
+            Debug.Log("Request successful: " + result);
+        }
+    }
+}
+
 }
