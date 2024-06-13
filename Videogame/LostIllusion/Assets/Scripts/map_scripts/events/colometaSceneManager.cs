3637f4b66129f474485138ea0a431bad82181cd0
sofia-lpz <a01028251@tec.mx>
2024-06-13 10:33:11 -0600

    minor changes to script

--- Videogame/LostIllusion/Assets/Scripts/map_scripts/events/colometaSceneManager.cs
+++ Videogame/LostIllusion/Assets/Scripts/map_scripts/events/colometaSceneManager.cs
@@ -9,39 +9,47 @@ public class colometaSceneManager : MonoBehaviour
     private npcMovement npcMovement;
     [SerializeField] public Transform[] points;
     [SerializeField] public AudioClip newBgMusicClip;
+    GameObject centipede;
 
     void Start()
     {
         if (stateNameController.match_CENTIPEDE == 1)
-        {
-            // Get the child object
-Transform childTransform = transform.Find("BgSoundManager");
-if (childTransform != null)
-{
-    // Get the BgSoundManager component
-    BgSoundManager bgSoundManager = childTransform.GetComponent<BgSoundManager>();
-    if (bgSoundManager != null)
+            {
+                // Get the child object
+    Transform childTransform = transform.Find("BgSoundManager");
+    if (childTransform != null)
     {
-        // Load a different bgMusic
-        bgSoundManager.bgMusic = newBgMusicClip;
-        // Restart the audio source to play the new bgMusic
-        bgSoundManager.audioSource1.Stop();
-        bgSoundManager.audioSource1.clip = newBgMusicClip;
-        bgSoundManager.audioSource1.Play();
+        // Get the BgSoundManager component
+        BgSoundManager bgSoundManager = childTransform.GetComponent<BgSoundManager>();
+        if (bgSoundManager != null)
+        {
+            // Load a different bgMusic
+            bgSoundManager.bgMusic = newBgMusicClip;
+            // Restart the audio source to play the new bgMusic
+            bgSoundManager.audioSource1.Stop();
+            bgSoundManager.audioSource1.clip = newBgMusicClip;
+            bgSoundManager.audioSource1.Play();
+        }
     }
-}
-            Debug.Log("Centipede match won");
-            GameObject.Find("CENTIPEDE").SetActive(false);
-            GameObject player = GameObject.FindWithTag("Player");
+                Debug.Log("Centipede match won");
+                GameObject.Find("CENTIPEDE").SetActive(false);
+                GameObject player = GameObject.FindWithTag("Player");
 
-            player.transform.position = new Vector3(0, 14, 0);
+                player.transform.position = new Vector3(0, 14, 0);
 
-            GameObject girl = Object.Instantiate(girlPrefab);
-            girl.name = "GIRL";
+                GameObject girl = Object.Instantiate(girlPrefab);
+                girl.name = "GIRL";
 
-            npcMovement = girl.GetComponent<npcMovement>();
-            npcMovement.points = points;
-            StartCoroutine(WaitForRoute());
+                npcMovement = girl.GetComponent<npcMovement>();
+                npcMovement.points = points;
+                StartCoroutine(WaitForRoute());
+        }
+        else
+        {
+            Debug.Log("Centipede match lost");
+            centipede = GameObject.Find("CENTIPEDE");
+            centipede.SetActive(true);
+            centipede.GetComponent<npcMovement>().enabled = true;
         }
         
     }
